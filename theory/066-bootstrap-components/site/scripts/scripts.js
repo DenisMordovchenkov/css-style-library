79e40e65b8609f19e1ffc8bbfe5e7fd2d123fa20
Denis Mordovchenkov <denisbabichev@gmail.com>
2025-10-05 12:17:40 +0300

    Rewrote the project for a future blog. Added basic markup and styles for clarity. Wrote the first post

--- theory/066-bootstrap-components/site/scripts/scripts.js
+++ theory/066-bootstrap-components/site/scripts/scripts.js
@@ -1,12 +1,1 @@
-function toggleTheme() {
-    const theme = document.documentElement.getAttribute('data-bs-theme');
-    const btn = document.getElementById('theme-toggle-btn');
 
-    if (theme === 'light') {
-        document.documentElement.setAttribute('data-bs-theme', 'dark');
-        btn.innerHTML = `🌙`;
-    } else {
-        document.documentElement.setAttribute('data-bs-theme', 'light');
-        btn.innerHTML = `☀️`;
-    }
-}
